c0858c1d078a6b05a64207f9e339851862f073f9
Edixon <edixonparraga@gmail.com>
2023-11-01 22:44:11 +0100

    segunda modificación de HBBQO.sh Opción_Menú

--- HBBQO.sh
+++ HBBQO.sh
@@ -8,3 +8,16 @@ fi
 
 base_de_datos="$1"
 
+enDesenvolupament () {
+  echo ----------------------------------
+  echo Base de Dades del catàleg de HBBQO
+  echo ----------------------------------
+  echo 1.- Llistats de les pel·lícules del catàleg.
+  echo 2.- Cerca d’una pel·lícula al catàleg.
+  echo 3.- Joc de preguntes sobre les pel·lícules.
+  echo 4.- Gestió de la base de dades de pel·lícules.
+  echo 0.- Sortir.
+  echo -e "Escull una opcio:\c"
+  read opcio
+}
+
