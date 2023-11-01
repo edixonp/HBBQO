f62fe1e466621253c459bea0840008c6f49c92d7
Edixon <edixonparraga@gmail.com>
2023-11-01 22:40:51 +0100

    primera modificación de HBBQO.sh Opción_Menú

--- HBBQO.sh
+++ HBBQO.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Comprobar si se proporcionó un argumento para el nombre de la base de datos
+if [ $# -ne 1 ]; then
+  echo "Uso: $0 <nombre-base-datos>"
+  exit 1
+fi
+
+base_de_datos="$1"
+
